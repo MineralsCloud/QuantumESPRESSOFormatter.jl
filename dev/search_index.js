9584bccb5d28e528c098d6e50a9c1e05f5e283f8
Documenter.jl <documenter@juliadocs.github.io>
2021-08-09 00:58:05 +0000

    build based on f01ffa6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumESPRESSOFormatter","category":"page"},{"location":"#QuantumESPRESSOFormatter","page":"Home","title":"QuantumESPRESSOFormatter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumESPRESSOFormatter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumESPRESSOFormatter]","category":"page"}]
+}
