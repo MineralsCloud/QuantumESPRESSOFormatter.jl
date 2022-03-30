c410320765145b1e4221a190feda11dacd4f2580
Reno <singularitti@outlook.com>
2022-03-30 10:18:12 -0400

    Add "Test `format_file`"

--- test/PWscf.jl
+++ test/PWscf.jl
@@ -3,7 +3,7 @@ module PWscf
 using StructArrays: StructArray
 using Test
 using QuantumESPRESSOBase.Inputs.PWscf: AtomicSpecies, AtomicPosition, AtomicSpeciesCard, AtomicPositionsCard
-using QuantumESPRESSOFormatter.Inputs.PWscf: format_text
+using QuantumESPRESSOFormatter.Inputs.PWscf: format_text, format_file
 
 @testset "Test constructing `AtomicSpeciesCard`" begin
     a = ["Al", "As"]
@@ -66,6 +66,13 @@ end
     8 8 8 1 1 1
     """
     @test format_text(str) == "&CONTROL\n    prefix = 'Fe'\n    outdir = 'tmp/'\n    pseudo_dir = 'pseudo/'\n/\n&SYSTEM\n    starting_magnetization(1) = 0.5\n    ecutwfc = 45.0\n    celldm(1) = 5.42\n    ecutfock = 300.0\n    occupations = 'smearing'\n    ntyp = 1\n    smearing = 'mp'\n    ibrav = 3\n    degauss = 0.04\n    nr2 = 27\n    nr3 = 27\n    lspinorb = .true.\n    ecutrho = 300.0\n    noncolin = .true.\n    nr1 = 27\n    nat = 1\n/\n&ELECTRONS\n    conv_thr = 1.0e-10\n/\n&IONS\n/\n&CELL\n/\nATOMIC_SPECIES\n      Fe    0.000000000 Fe.rel-pbe-kjpaw.UPF\nATOMIC_POSITIONS { alat }\n      Fe    0.000000000    0.000000000    0.000000000\nK_POINTS { automatic }\n        8     8     8     1     1     1\n\n\n\n\n\n"
+end
+
+# Data from https://gitlab.com/QEF/q-e/-/blob/a463c379/PW/examples/example10/run_example#L82-115
+@testset "Test `format_file`" begin
+    file = "si.scf.efield.in"
+    format_file(file)
+    read(file, String) == "&CONTROL\n    prefix = 'silicon'\n    outdir = 'tmp/'\n    pseudo_dir = 'pseudo/'\n    lelfield = .true.\n/\n&SYSTEM\n    nat = 8\n    ecutwfc = 20.0\n    celldm(1) = 10.18\n    ecutrho = 80.0\n    ecutfock = 80.0\n    ntyp = 1\n    ibrav = 1\n/\n&ELECTRONS\n    mixing_beta = 0.5\n    conv_thr = 1.0e-8\n    startingwfc = 'random'\n/\n&IONS\n/\n&CELL\n/\nATOMIC_SPECIES\n      Si   28.086000000 Si.pbe-rrkj.UPF\nATOMIC_POSITIONS { alat }\n      Si   -0.125000000   -0.125000000   -0.125000000\n      Si    0.375000000    0.375000000   -0.125000000\n      Si    0.375000000   -0.125000000    0.375000000\n      Si   -0.125000000    0.375000000    0.375000000\n      Si    0.125000000    0.125000000    0.125000000\n      Si    0.625000000    0.625000000    0.125000000\n      Si    0.625000000    0.125000000    0.625000000\n      Si    0.125000000    0.625000000    0.625000000\nK_POINTS { automatic }\n        3     3     7     0     0     0\n\n\n\n\n\n"
 end
 
 end
