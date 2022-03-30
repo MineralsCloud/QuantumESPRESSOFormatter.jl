1624d37352d766f7eed9d5de5e46ae5f1c507732
Reno <singularitti@outlook.com>
2022-03-30 09:53:38 -0400

    Change "Test `format_text`"

--- test/PWscf.jl
+++ test/PWscf.jl
@@ -30,37 +30,42 @@ end
           "ATOMIC_POSITIONS { alat }\n       S    0.500000000    0.288675130    1.974192764\n      Mo    0.000000000    0.577350270    2.462038339\n       S    0.000000000   -0.577350270    2.950837559"
 end
 
+# Data from https://gitlab-hpc.cineca.it/pbonfa01/q-e/-/blob/c9f92947/PW/examples/example11/run_example#L86-117
 @testset "Test `format_text`" begin
     str = raw"""
-    &control
-    calculation='scf'
-    pseudo_dir  = '/home/qe/pseudo'
-    prefix='GaN'
-    outdir      = './tmp'
-    /
-    &system
-    ibrav = 4
-    celldm(1)=5.95484286816
-    celldm(3)=1.63011343669
-    nat = 4
-    ntyp = 2
-    ecutwfc = 160
-    /
-    &electrons
-    conv_thr=1.0d-10
-    /
+     &control
+        calculation = 'scf'
+    prefix='Fe',
+        pseudo_dir = 'pseudo/',
+        outdir='tmp/'
+     /
+     &system
+        ibrav=  3,
+        celldm(1) =5.42,
+        nat=  1,
+        ntyp= 1,
+        nr1=27,
+        nr2=27,
+        nr3=27,
+        noncolin=.true.
+        lspinorb=.true.
+        starting_magnetization(1)=0.5,
+        occupations='smearing',
+        smearing='mp',
+        degauss=0.04,
+        ecutwfc =45.0, ecutrho =300.0
+     /
+     &electrons
+        conv_thr =  1.0d-10
+     /
     ATOMIC_SPECIES
-    Ga  69.723   Ga.pbe-dn-kjpaw_psl.1.0.0.UPF
-    N   14.007   N.pbe-n-kjpaw_psl.1.0.0.UPF
-    ATOMIC_POSITIONS (crystal)
-    Ga       0.666666667   0.333333333  -0.000051966
-    N        0.666666667   0.333333333   0.376481188
-    Ga       0.333333333   0.666666667   0.499948034
-    N        0.333333333   0.666666667   0.876481188
-    K_POINTS automatic
-    6 6 6 1 1 1
+    Fe  0.0    Fe.rel-pbe-kjpaw.UPF
+    ATOMIC_POSITIONS   alat
+    Fe  0.0000000   0.00000000   0.0
+    K_POINTS AUTOMATIC
+    8 8 8 1 1 1
     """
-    @test format_text(str) == "&CONTROL\n    prefix = 'GaN'\n    outdir = './tmp'\n    pseudo_dir = '/home/qe/pseudo'\n/\n&SYSTEM\n    nat = 4\n    ecutwfc = 160.0\n    celldm(1) = 5.95484286816\n    celldm(3) = 1.63011343669\n    ecutrho = 640.0\n    ecutfock = 640.0\n    ntyp = 2\n    ibrav = 4\n/\n&ELECTRONS\n    conv_thr = 1.0e-10\n/\n&IONS\n/\n&CELL\n/\nATOMIC_SPECIES\n      Ga   69.723000000 Ga.pbe-dn-kjpaw_psl.1.0.0.UPF\n       N   14.007000000 N.pbe-n-kjpaw_psl.1.0.0.UPF\nATOMIC_POSITIONS { crystal }\n      Ga    0.666666667    0.333333333   -0.000051966\n       N    0.666666667    0.333333333    0.376481188\n      Ga    0.333333333    0.666666667    0.499948034\n       N    0.333333333    0.666666667    0.876481188\nK_POINTS { automatic }\n        6     6     6     1     1     1\n\n\n\n\n\n"
+    @test format_text(str) == "&CONTROL\n    prefix = 'Fe'\n    outdir = 'tmp/'\n    pseudo_dir = 'pseudo/'\n/\n&SYSTEM\n    starting_magnetization(1) = 0.5\n    ecutwfc = 45.0\n    celldm(1) = 5.42\n    ecutfock = 300.0\n    occupations = 'smearing'\n    ntyp = 1\n    smearing = 'mp'\n    ibrav = 3\n    degauss = 0.04\n    nr2 = 27\n    nr3 = 27\n    lspinorb = .true.\n    ecutrho = 300.0\n    noncolin = .true.\n    nr1 = 27\n    nat = 1\n/\n&ELECTRONS\n    conv_thr = 1.0e-10\n/\n&IONS\n/\n&CELL\n/\nATOMIC_SPECIES\n      Fe    0.000000000 Fe.rel-pbe-kjpaw.UPF\nATOMIC_POSITIONS { alat }\n      Fe    0.000000000    0.000000000    0.000000000\nK_POINTS { automatic }\n        8     8     8     1     1     1\n\n\n\n\n\n"
 end
 
 end
